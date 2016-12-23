939ea176d573eca4fe64ed0c77a3b75b8ae94109
Daniel Melani <daniel.melani@gmail.com>
2016-12-23 20:01:35 +0100

    Toss in some simple RAM.
    I believe nes uses 2k RAM

--- cpu.c
+++ cpu.c
@@ -5,10 +5,30 @@
 #include "cpu.h"
 #include "util.h"
 
+#define NES_RAM_SIZE 2048
+
 cpu *
 cpu_create() {
-	cpu *cpu = xmalloc(sizeof(*cpu));	
-	memset(cpu, 0, sizeof(*cpu));
+	cpu *c = xmalloc(sizeof(*c));	
+	c->ram  = xmalloc(NES_RAM_SIZE);
 
-	return cpu;
+	cpu_reset(c);
+
+	return c;
+}
+
+void
+cpu_destroy(cpu *c) {
+	free(c->ram);
+	free(c);
+}
+
+void
+cpu_reset(cpu *c) {
+	char *ram = c->ram;
+
+	memset(c->ram, 0, NES_RAM_SIZE);
+	memset(c, 0, sizeof(*c));
+
+	c->ram = ram;
 }
--- cpu.h
+++ cpu.h
@@ -5,8 +5,21 @@ typedef struct {
 	uint8_t a;	// Accumulator
 	uint8_t x;	// Index X
 	uint8_t y;	// Index Y
-	uint8_t p;	// Flags
 	uint8_t s;	// Stack pointer
+
+#define CPU_FLAG_C 1 << 0 // Carry
+#define CPU_FLAG_Z 1 << 1 // Zero
+#define CPU_FLAG_I 1 << 2 // Interrupt disable
+#define CPU_FLAG_D 1 << 3 // Binary coded decimal
+#define CPU_FLAG_B 1 << 4 // Breakpoint
+#define CPU_FLAG_U 1 << 5 // Unused
+#define CPU_FLAG_V 1 << 6 // Overflow
+#define CPU_FLAG_N 1 << 7 // Sign
+	uint8_t p;	// Flags
+
+	char *ram;
 } cpu;
 
 cpu * cpu_create();
+void cpu_destroy(cpu*);
+void cpu_reset(cpu*);
