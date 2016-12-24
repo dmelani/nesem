3e611c8cba52e2a26f32b49b45f4528e8ffebcda
Daniel Melani <daniel.melani@gmail.com>
2016-12-24 21:59:52 +0100

    Think reset and powerup cpu state is alright now.

--- cpu.c
+++ cpu.c
@@ -5,14 +5,19 @@
 #include "cpu.h"
 #include "util.h"
 
-#define NES_RAM_SIZE 2048
+#define NES_MEM_MAP_SIZE 0x8000
+#define NES_RAM_SIZE 0x0800
 
+/* Local function declarations */
+static void cpu_powerup(cpu *);
+
+/* Global functions */
 cpu *
 cpu_create() {
 	cpu *c = xmalloc(sizeof(*c));	
-	c->ram  = xmalloc(NES_RAM_SIZE);
+	c->ram  = xmalloc(NES_MEM_MAP_SIZE);
 
-	cpu_reset(c);
+	cpu_powerup(c);
 
 	return c;
 }
@@ -25,10 +30,24 @@ cpu_destroy(cpu *c) {
 
 void
 cpu_reset(cpu *c) {
-	char *ram = c->ram;
+	/* As per https://wiki.nesdev.com/w/index.php/CPU_power_up_state */
+	c->s -= 3;
+	c->p |= CPU_FLAG_I;
 
-	memset(c->ram, 0, NES_RAM_SIZE);
-	memset(c, 0, sizeof(*c));
+}
 
-	c->ram = ram;
+/* Local function definitions */
+
+static void
+cpu_powerup(cpu *c) {
+	/* As per https://wiki.nesdev.com/w/index.php/CPU_power_up_state */
+
+	c->a = 0;
+	c->x = 0;
+	c->y = 0;
+	c->s = 0xFD;
+	c->p = CPU_FLAG_I | CPU_FLAG_B | CPU_FLAG_U;
+
+	memset(c->ram, 0x00, NES_MEM_MAP_SIZE);
+	memset(c->ram, 0xFF, NES_RAM_SIZE);
 }
