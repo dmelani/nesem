2e87957a09f2b200f4367431329517487c6d22ec
Daniel Melani <daniel.melani@gmail.com>
2016-12-28 19:51:26 +0100

    Make flags unsigned

--- src/cpu.h
+++ src/cpu.h
@@ -11,14 +11,14 @@ typedef struct cpu {
 	uint8_t y;	// Index Y
 	uint8_t s;	// Stack pointer
 
-#define CPU_FLAG_C 1 << 0 // Carry
-#define CPU_FLAG_Z 1 << 1 // Zero
-#define CPU_FLAG_I 1 << 2 // Interrupt disable
-#define CPU_FLAG_D 1 << 3 // Binary coded decimal
-#define CPU_FLAG_B 1 << 4 // Breakpoint
-#define CPU_FLAG_U 1 << 5 // Unused
-#define CPU_FLAG_V 1 << 6 // Overflow
-#define CPU_FLAG_N 1 << 7 // Sign
+#define CPU_FLAG_C 1U << 0 // Carry
+#define CPU_FLAG_Z 1U << 1 // Zero
+#define CPU_FLAG_I 1U << 2 // Interrupt disable
+#define CPU_FLAG_D 1U << 3 // Binary coded decimal
+#define CPU_FLAG_B 1U << 4 // Breakpoint
+#define CPU_FLAG_U 1U << 5 // Unused
+#define CPU_FLAG_V 1U << 6 // Overflow
+#define CPU_FLAG_N 1U << 7 // Sign
 	uint8_t p;	// Flags
 
 	uint8_t *mem;
