311fa41a9b0a26564992f32bdfdfe13dfe928f9d
Daniel Melani <daniel.melani@gmail.com>
2016-12-29 15:20:11 +0100

    STA ZPX

--- src/isa/sta.c
+++ src/isa/sta.c
@@ -26,6 +26,20 @@ sta_indirect_indexed(cpu *c, addressing_mode am) {
 	cpu_write(c, addr, c->a);
 }
 
+static void
+sta_zero_page_x(cpu *c, addressing_mode am) {
+	uint8_t zpa = cpu_advance(c);
+	uint8_t addr;
+
+	/* This is what it does according to http://nesdev.com/6502_cpu.txt */
+	cpu_read(c, zpa);
+	addr = zpa + c->x;
+
+	printf("\tSTA (0x%0.2x),0x%0.2x: A 0x%0.2x to addr 0x%0.4x\n", zpa, c->x, c->a, addr);
+	cpu_write(c, addr, c->a);
+}
+
 ADD_INSTRUCTION(0x8d, STA, ABSOLUTE, sta);
 ADD_INSTRUCTION(0x91, STA, INDIRECT_INDEXED, sta_indirect_indexed);
+ADD_INSTRUCTION(0x95, STA, INDEXED_ZERO_PAGE_X, sta_zero_page_x);
 
