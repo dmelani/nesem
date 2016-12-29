a232cab5572eb6dc762c4bb8c3c3e99d2d724de8
Daniel Melani <daniel.melani@gmail.com>
2016-12-29 08:29:49 +0100

    sta indirect indexed

--- src/isa/sta.c
+++ src/isa/sta.c
@@ -9,10 +9,24 @@ sta(cpu *c, addressing_mode am) {
 	uint8_t low = cpu_advance(c);
 	uint8_t high = cpu_advance(c);
 	uint16_t addr = low | (high << 8);
+
 	printf("\tSTA: A 0x%0.2x to addr 0x%0.4x\n", c->a, addr);
 
 	cpu_write(c, addr, c->a);
 }
 
+static void
+sta_indirect_indexed(cpu *c, addressing_mode am) {
+	uint8_t zpa = cpu_advance(c);
+	uint16_t addr = cpu_read(c, zpa);
+	addr += c->y;
+	cpu_tick_clock(c); /* Takes one cycle more */
+
+	printf("\tSTA (0x%0.2x),0x%0.2x: A 0x%0.2x to addr 0x%0.4x\n", zpa, c->y, c->a, addr);
+	cpu_write(c, addr, c->a);
+	cpu_tick_clock(c); /* Takes one cycle more */
+}
+
 ADD_INSTRUCTION(0x8d, STA, ABSOLUTE, sta);
+ADD_INSTRUCTION(0x91, STA, INDIRECT_INDEXED, sta_indirect_indexed);
 
