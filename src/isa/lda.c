d8fe386415c832d7f485420fa433e330092884dc
buggo <rikard.b.karlsson@gmail.com>
2016-12-29 11:45:05 +0100

    LDA Absolute

--- src/isa/lda.c
+++ src/isa/lda.c
@@ -7,6 +7,15 @@
 static void
 lda(cpu *c, addressing_mode am) {
 	uint8_t data = cpu_advance(c);
+
+	if (am == ABSOLUTE) {
+		uint16_t addr = data | (cpu_advance(c) << 8);
+		printf("\tLDA: addr: 0x%0.4x\n", addr);
+		data = cpu_read(c, addr);
+		cpu_tick_clock(c); /* Takes two more cycles */
+		cpu_tick_clock(c);
+	}
+
 	c->a = data;
 
 	cpu_set_n(c, data);
@@ -16,4 +25,5 @@ lda(cpu *c, addressing_mode am) {
 }
 
 ADD_INSTRUCTION(0xa9, LDA, IMMEDIATE, lda);
+ADD_INSTRUCTION(0xad, LDA, ABSOLUTE, lda);
 
