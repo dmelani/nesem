857033d553bd58bb825162abed1336ad914ed27a
Daniel Melani <daniel.melani@gmail.com>
2016-12-30 14:47:05 +0100

    LDA zero page

--- src/isa/lda.c
+++ src/isa/lda.c
@@ -7,11 +7,15 @@
 static void
 lda(cpu *c, addressing_mode am) {
 	uint8_t data = cpu_advance(c);
+	printf("\tLDA ");
 
 	if (am == ABSOLUTE) {
 		uint16_t addr = data | (cpu_advance(c) << 8);
-		printf("\tLDA: addr: 0x%0.4x\n", addr);
+		printf("(0x%0.4x)", addr);
 		data = cpu_read(c, addr);
+	} else if (am == ZERO_PAGE) {
+		printf("(0x%0.2x)", data);
+		data = cpu_read(c, data);
 	}
 
 	c->a = data;
@@ -19,9 +23,10 @@ lda(cpu *c, addressing_mode am) {
 	cpu_set_n(c, data);
 	cpu_set_z(c, data);
 
-	printf("\tLDA: 0x%0.2x Flags: 0x%0.2x\n", data, c->p);
+	printf(": 0x%0.2x Flags: 0x%0.2x\n", data, c->p);
 }
 
 ADD_INSTRUCTION(0xa9, LDA, IMMEDIATE, lda);
 ADD_INSTRUCTION(0xad, LDA, ABSOLUTE, lda);
+ADD_INSTRUCTION(0xa5, LDA, ZERO_PAGE, lda);
 
