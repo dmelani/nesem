135939fa007da182397ba9369ad538bbe536956e
buggo <rikard.b.karlsson@gmail.com>
2016-12-31 10:18:29 +0100

    LDA indexed absolute x

--- src/isa/lda.c
+++ src/isa/lda.c
@@ -16,6 +16,15 @@ lda(cpu *c, addressing_mode am) {
 	} else if (am == ZERO_PAGE) {
 		printf("(0x%0.2x)", data);
 		data = cpu_read(c, data);
+	} else if (am == INDEXED_ABSOLUTE_X) {
+		uint8_t high = cpu_advance(c); //data | (cpu_advance(c) << 8);
+		uint16_t addr = (data | (high << 8)) + c->x;
+		if (addr >> 8 != high) {
+			printf("+");
+			cpu_read(c, (high << 8) | (addr & 0xFF));
+		}
+		printf("(0x%0.4x)", addr);
+		data = cpu_read(c, addr);
 	}
 
 	c->a = data;
@@ -29,3 +38,4 @@ lda(cpu *c, addressing_mode am) {
 ADD_INSTRUCTION(0xa5, LDA, ZERO_PAGE, lda);
 ADD_INSTRUCTION(0xa9, LDA, IMMEDIATE, lda);
 ADD_INSTRUCTION(0xad, LDA, ABSOLUTE, lda);
+ADD_INSTRUCTION(0xbd, LDA, INDEXED_ABSOLUTE_X, lda);
