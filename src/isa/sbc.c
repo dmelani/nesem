991a8371da3206a82abfbf17ddb32bbc32bf5f2c
Daniel Melani <daniel.melani@gmail.com>
2017-01-02 20:56:03 +0100

    Full sbc

--- src/isa/sbc.c
+++ src/isa/sbc.c
@@ -6,11 +6,12 @@
 
 static void
 sbc(cpu *c, addressing_mode am) {
-	uint8_t data = cpu_advance(c);
+	printf("\tSBC: ");
+	uint8_t data = isa_load_read(c, am);
 	uint8_t carry = c->p & CPU_FLAG_C;
 	uint16_t tmp = c->a - data - (c ? 0 : 1);
 
-	printf("\tSBC: 0x%0.2x - 0x%0.2x - 0x%0.1x = ", c->a, data, carry ? 0 : 1);
+	printf("0x%0.2x - 0x%0.2x - 0x%0.1x = ", c->a, data, carry ? 0 : 1);
 
 	cpu_set_n(c, tmp & 0xFF);
 	cpu_set_z(c, tmp & 0xFF);
@@ -32,4 +33,11 @@ sbc(cpu *c, addressing_mode am) {
 }
 
 ADD_INSTRUCTION(0xe9, SBC, IMMEDIATE, sbc);
+ADD_INSTRUCTION(0xe5, SBC, ZERO_PAGE, sbc);
+ADD_INSTRUCTION(0xf5, SBC, INDEXED_ZERO_PAGE_X, sbc);
+ADD_INSTRUCTION(0xed, SBC, ABSOLUTE, sbc);
+ADD_INSTRUCTION(0xfd, SBC, INDEXED_ABSOLUTE_X, sbc);
+ADD_INSTRUCTION(0xf9, SBC, INDEXED_ZERO_PAGE_Y, sbc);
+ADD_INSTRUCTION(0xe1, SBC, INDEXED_INDIRECT, sbc);
+ADD_INSTRUCTION(0xf1, SBC, INDIRECT_INDEXED, sbc);
 
